9a4ea36496067930905231323160429d800a25d4
rotary emanoel <rotary@mbp-de-rotary.home>
2022-06-16 09:43:20 +0200

    first quete: introduction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'myFirstAngularApp';
+  title = 'Bienvenue sur le site de Chloé !';
 }
