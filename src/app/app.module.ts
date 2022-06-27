3d8189164712c5496a5d72138b5cbbee0289a4b5
chloe azerty <chloe.rotaru@yahoo.com>
2022-06-27 11:16:34 +0200

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { KitchenComponent } from './kitchen/kitchen.component';
-import { MagicOvenComponent } from './magic-oven/magic-oven.component';
 import { DeveloperComponent } from './developer/developer.component';
 import { SkillComponent } from './skill/skill.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, KitchenComponent, MagicOvenComponent, DeveloperComponent, SkillComponent],
+  declarations: [ AppComponent,  DeveloperComponent, SkillComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
